8445299217aaf33931e73816a6236602e27e54a1
John Evans <john@grandadevans.com>
2022-09-17 19:25:37 +0000

    Back to default app name, & tests green

---
+++ app/Utilities/Filesystem.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Utilities;
+
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Filesystem\Filesystem as SymfonyFilesystem;
+
+class Filesystem
+{
+
+    /**
+     * Set the base directory for the user's filesystem to go into
+     */
+    public static $defaultUserFilesystems = 'UserFilesystems/';
+
+
+    /**
+     * Get the md5 hash for a directory
+     *
+     * @param $dir String   The directory to create the user filesystem in
+     *
+     * @return String
+     */
+    public static function md5Dir(String $dir) : String
+    {
+        return self::md5DirRecursively($dir);
+    }
+
+
+    /**
+     * Create the new user's filesystem from a skeleton version
+     *
+     * @param $userUUID String? If supplied (normally from the user object) it gived the user's uinque id
+     *
+     * @return void
+     */
+    public static function createNewUserFilesystem(String $userUUID = null) : void
+    {
+        if (is_null($userUUID)) {
+            $userUUID = Uuid::uuid4()->toString();
+        }
+
+        $fs = new SymfonyFilesystem();
+        $skeletonDir = 'tests/Resources/RecursiveTestSrc';
+        $baseDir = self::$defaultUserFilesystems;
+        $userDir = "${baseDir}/${userUUID}";
+
+        try {
+            $fs->mirror($skeletonDir, $userDir);
+        }
+        catch(\Exception $e) {
+            throw new FilesystemEzxeption('Unable to create your default user space');
+        }
+    }
+
+    /**
+     * Get the MD5 hash of an entire directory
+     *
+     * Taken from https://www.php.net/manual/en/function.md5-file.php#75393
+     */
+    private static function md5DirRecursively($dir)
+    {
+        if (!is_dir($dir))
+        {
+            return false;
+        }
+
+        $filemd5s = array();
+        $d = dir($dir);
+
+        while (false !== ($entry = $d->read()))
+        {
+            if ($entry != '.' && $entry != '..')
+            {
+                 if (is_dir($dir.'/'.$entry))
+                 {
+                     $filemd5s[] = self::md5DirRecursively($dir.'/'.$entry);
+                 }
+                 else
+                 {
+                     $filemd5s[] = md5_file($dir.'/'.$entry);
+                 }
+             }
+        }
+        $d->close();
+        return md5(implode('', $filemd5s));
+    }
+}
---
+++ tests/Unit/FilesystemTests.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\Filesystem\Filesystem as SymfonyFilesystem;
+use App\Utilities\Filesystem;
+
+class FilesystemTests extends TestCase
+{
+    /**
+     * Test we get a correct file hash for an empty directory
+     * 
+     * @test
+     * 
+     * @return void
+     */
+    public function make_sure_we_get_a_directory_hash_correctly() : void
+    {
+        $path = './tests/Resources/RecursiveTestSrc/test-dir-1/empty-directory';
+
+        $md5 = Filesystem::md5Dir($path);
+        $this->assertEquals('d41d8cd98f00b204e9800998ecf8427e', $md5);
+    }
+
+    /**
+     * test user gets an exact copy of skeleton directory
+     * 
+     * @depends make_sure_we_get_a_directory_hash_correctly
+     * @test
+     *
+     * @return void
+     */
+    public function make_sure_we_can_copy_the_skeleton_user_directory() : void
+    {
+        $filesystem = new SymfonyFilesystem();
+
+        // Make sure we have the default test directories
+        $src = './tests/Resources/RecursiveTestSrc';
+        $dst = './tests/Resources/RecursiveTestDst';
+        // Temp. set the destintion dir for the test
+        Filesystem::$defaultUserFilesystems = $dst;
+
+        // Assert that both base directories already exist
+        $this->assertTrue($filesystem->exists($src));
+        $this->assertTrue($filesystem->exists($dst));
+
+        // Create a new UUID (normally it will coime from user)
+        $uuid = Uuid::uuid4()->toString();
+        //  Set the new destination dir
+        $newDst = "${dst}/${uuid}";
+        // Ensure it doesn't already exist
+        $this->assertFalse($filesystem->exists($newDst));
+
+        // Perform the magic
+        Filesystem::createNewUserFilesystem($uuid);
+
+        // Assert it has been coppied acrosss
+        $this->assertTrue($filesystem->exists($src));
+        $this->assertTrue($filesystem->exists($newDst));
+
+        // Confirm they both match
+        $md5Src = Filesystem::md5Dir($src);
+        $md5Dst = Filesystem::md5Dir($newDst);
+        $this->assertEquals($md5Src, $md5Dst);
+    }
+}
